837ca8957c36d0626f7f7e1a8965ba0d8693c615
root <root@ip-172-32-6-93.ap-east-1.compute.internal>
2026-06-29 07:57:00 +0000

    deploy

---
+++ config.js
@@ -0,0 +1,20 @@
+window.LIVE_ROOMS = [
+  {
+    name: 'babu',
+    title: '巴布社区直播间',
+    poster: 'assets/live1/13.png',
+    link: 'https://meeting.tencent.com/dm/RbhpeVk7B4B4',
+  },
+  {
+    name: 'ruiying',
+    title: '瑞应社区直播间',
+    poster: 'assets/live1/23.png',
+    link: 'https://meeting.tencent.com/dm/vJ0Vey6yfz77',
+  },
+  // {
+  //   name: 'lianke',
+  //   title: '链客社区直播间',
+  //   poster: 'assets/live1/31.png',
+  //   link: 'https://app.binance.com/uni-qr/cspa/42234211071386?l=zh-CN&r=KCZ9AOOX&source=host_share&uc=web_square_share_link&us=copylink',
+  // },
+];
