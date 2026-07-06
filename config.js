ab6b054078a8090d56d90526492daea7cc2b10cf
tiyaaaa-ai <yangzzz283@gmail.com>
2026-07-06 18:43:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ window.LIVE_ROOMS = [
     link: 'https://meeting.tencent.com/dm/szM2WYP9rONA',
   },
   
-  // {
-  //   name: 'lianke',
-  //   title: '链客社区直播间',
-  //   poster: 'assets/live1/31.png',
-  //   link: 'https://app.binance.com/uni-qr/cspa/42234211071386?l=zh-CN&r=KCZ9AOOX&source=host_share&uc=web_square_share_link&us=copylink',
-  // },
+  {
+    name: 'qingfeng',
+    title: '清风社区直播间',
+    poster: 'assets/live1/33.png',
+    link: 'https://www.binance.com/zh-CN/square/audio?id=42720980792242',
+  },
   
   // {
   //   name: 'qianfeng',
