8ca11261fd333b66b5769c5cb95cff57bd007fb0
lakes001 <lakes@ave.ai>
2026-07-24 20:42:50 +0800

    Update link for ruiying community livestream

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ window.LIVE_ROOMS = [
     name: 'ruiying',
     title: '瑞应社区直播间',
     poster: 'assets/live1/11.png',
-    link: 'https://meeting.tencent.com/dm/Vc3K7FQ7zJav',
+    link: 'https://meeting.tencent.com/dm/fgtNSJIEc8Dv',
   },
   
   // {
