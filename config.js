e67461158b45a5e9586ee8114b5056a74bc1d441
tiyaaaa-ai <yangzzz283@gmail.com>
2026-09-14 20:58:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.LIVE_ROOMS = [
     name: 'babu',
     title: '巴布社区直播间',
     poster: 'assets/live1/66.png',
-    link: 'https://meeting.tencent.com/dm/QCV7WuIixUIJ',
+    link: 'https://meeting.tencent.com/dm/z4Vh9lOitPrY',
   },
   // {
   //   name: 'ruiying',
